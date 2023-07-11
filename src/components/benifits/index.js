7accb8598a322ca75bc5ad363a41942728bbb5f3
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-11 11:05:24 +0600

    body video added

--- src/components/benifits/index.js
+++ src/components/benifits/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import benifit from "../../assets/images/benifit.png";
+// import benifit from "../../assets/images/benifit.png";
+
+import benifitVideo from "../../assets/videos/body.mp4";
 
 const Benifits = () => {
   return (
     <div>
-      <div className="flex md:flex-row flex-col w-full justify-between items-center benifits">
+      <div className="flex md:flex-row flex-col w-full justify-between items-center benifits relative">
         <div className="md:w-[45%] md:pl-[10%] md:pr-[13%] px-[30px] bg-[#252D43]">
           <div className="line w-[1px] h-[138px] bg-[#665F57] mb-[100px]"></div>
           <h2 className="text-5xl text-[#83895C]">Benefits</h2>
@@ -18,8 +20,17 @@ const Benifits = () => {
           <div className="line w-[1px] h-[138px] bg-[#665F57]"></div>
         </div>
 
-        <div className="video md:hidden block">
+        {/* <div className="video md:hidden block">
           <img src={benifit} alt="benifit" className="w-full" />
+        </div> */}
+        <div className="video block md:w-[55%] w-full right-0 top-0 md:absolute h-full object-cover">
+          <video
+            src={benifitVideo}
+            autoPlay
+            loop
+            muted
+            className="object-cover h-full"
+          ></video>
         </div>
       </div>
     </div>
